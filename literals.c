2ba589ae215fc03a66b09b76dba49f47e015ab92
Anil Shrestha <126516808+empath1928@users.noreply.github.com>
2025-05-09 11:02:55 +0545

    Add files via upload
    Literals = Assigning value to a variables

---
+++ literals.c
@@ -0,0 +1,11 @@
+// Write a program to take input of student as roll number, name, age and gender
+#include <stdio.h>
+int main () {
+	int roll=5, age=19;
+	char name[20]="Anil", gender[6]="Male";
+	
+	printf("\n Name : %s \n Gender : %s ", name, gender);
+	printf("\n Age :  %d \n Roll No : %d", age, roll);
+	
+	return 0;
+}
